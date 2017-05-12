1761e1288720db80530d9626705f4811c317da0c
Siddhant Bhakar <sidbhakar@gmail.com>
2017-05-12 17:41:47 +0000

    Implemented Trie struct with insertion and search

---
+++ speller/dictionary.c
@@ -0,0 +1,106 @@
+/**
+ * Implements a dictionary's functionality.
+ */
+
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+// struct for a node of trie
+typedef struct trienode
+{
+    bool isleaf; 
+    struct trienode *children[27];
+}trienode;
+
+trienode *root; // root of the trie
+
+// returns a new trie node with its children initialised to NULL
+
+trienode *newnode(void)
+{
+    trienode *node = (trienode *)malloc(sizeof(trienode));
+    int i;
+    for(i = 0 ; i < 27 ; i++)
+    {
+        node->children[i] = NULL;
+    }
+    return node;
+}
+
+//inserts a string in the trie
+
+void insert(char *word)
+{
+    int index,i;
+    trienode *temp = root;
+    int n = strlen(word);
+    for(i = 0 ; i < n ; i++)
+    {
+        if(word[i] != '\'')
+        {
+            index = word[i] - 'a';
+        }
+        else
+        {
+            index = 26;
+        }
+        if(temp->children[index] == NULL)
+        {
+            temp->children[index] = newnode();
+        }
+        temp = temp->children[index];
+    }
+    temp->isleaf = true;
+}
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word)
+{
+    trienode *temp = root;
+    int index, i, n;
+    n = strlen(word);
+    for(i = 0 ; i < 27 ; i++)
+    {
+        if(word[i] != '\'')
+        {
+            index = word[i] - 'a';
+        }
+        else
+        {
+            index = 26;
+        }
+        if(temp->children[index] == NULL && temp->isleaf == false)
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Loads dictionary into memory. Returns true if successful else false.
+ */
+bool load(const char *dictionary)
+{
+    // TODO
+    return false;
+}
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+/**
+ * Unloads dictionary from memory. Returns true if successful else false.
+ */
+bool unload(void)
+{
+    // TODO
+    return false;
+}
